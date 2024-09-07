ba2a01113a9e512ed44f1ed2c8a2acdd247bbd65
Vishal Gupta <63388692+vishalguptax@users.noreply.github.com>
2024-09-07 18:47:30 +0530

    fix: api url

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -4,7 +4,9 @@ import { redirect } from "next/navigation";
 
 const getCode = async (id: string) => {
   try {
-    const { data } = await axios.get(`http://localhost:3000/api/codes/${id}`);
+    const { data } = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/codes/${id}`
+    );
     return data;
   } catch (error) {
     return null;
@@ -14,9 +16,10 @@ const getCode = async (id: string) => {
 const Code = async ({ params }: { params: { id: string } }) => {
   const code = await getCode(params.id);
 
-  if (!code?.id || !!!code?.code) {
-    return redirect("/");
+  if (!code || !code.id || !code.code) {
+    redirect("/");
   }
+
   return <CodeViewer code={code} />;
 };
 
